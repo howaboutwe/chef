ab7f51e9d8c54e6da5ef591535103b7b375045b1
Matthew Vermaak and Scott Likens <dev@howaboutwe.com>
2012-03-07 15:25:26 +0000

    Modify the bootstrap_context so instances can come up easily.  No we don't write gems in /usr/bin any longer. Signed-off-by: Matthew Vermaak and Scott Likens <dev@howaboutwe.com>

--- chef/lib/chef/knife/core/bootstrap_context.rb
+++ chef/lib/chef/knife/core/bootstrap_context.rb
@@ -80,7 +80,7 @@ CONFIG
         end
 
         def start_chef
-          s = "/usr/bin/chef-client -j /etc/chef/first-boot.json"
+          s = "`which chef-client` -j /etc/chef/first-boot.json"
           s << " -E #{bootstrap_environment}" if chef_version.to_f != 0.9 # only use the -E option on Chef 0.10+
           s
         end
